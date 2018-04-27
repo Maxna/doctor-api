cdb82b85ed109bbf3ed59e5809bb1e4e8234761f
Matt Nardoni <nardoni59@gmail.com>
2018-04-27 12:04:42 -0700

    Add dotenv requirement

--- src/main.js
+++ src/main.js
@@ -2,20 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { doctor } from './../src/doctor.js';
+require('Dotenv').config();
 
 $(document).ready(function() {
   $('#showDr').click(function() {
     // event.preventDefault();
     let query = $('#findDr').val();
     $('#findDr').val("");
+    var apiKey = process.env.apiKey;
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=5&user_key=`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=5&user_key=apiKey`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        $('#output').text(`${response.data[0].profile.first_name}`);
+        $('#output').append('<h5>'+`${response.data[0].profile.first_name}`+'</h5>');
+        $('#output').append('<h5>'+`${response.data[0].profile.last_name}`+'</h5>');
+        $('#output').append(`${response.data[0].practices[0].visit_address.street}`);
+        $('#output').append(`${response.data[0].practices[0].phones[0].number}`);
+        $('#output').append(`${response.data[0].practices[0].accepts_new_patients}`);
       },
       error: function() {
         $('#errors').text("There was an error processing your request. Please try again.")
