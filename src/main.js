0db09fa1923a8ee7a0427227821b5f51db64bd73
Matt Nardoni <nardoni59@gmail.com>
2018-04-27 11:26:50 -0700

    Add working api call for first name

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import { doctor } from './../src/doctor.js';
 
 $(document).ready(function() {
-  $('#findDoctor').click(function() {
+  $('#showDr').click(function() {
+    // event.preventDefault();
+    let query = $('#findDr').val();
+    $('#findDr').val("");
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=sore%20throat&location=or-portland&skip=0&limit=10&user_key=`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=5&user_key=`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        $('#output').prepend(`${response.data[0].practices}`)
+        $('#output').text(`${response.data[0].profile.first_name}`);
       },
       error: function() {
-        $('#output').text("There was an error processing your request. Please try again.")
+        $('#errors').text("There was an error processing your request. Please try again.")
       }
     });
   });
