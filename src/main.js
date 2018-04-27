5a9afa24a2dd17f31fba8d85aec57d2eec51ed49
Matt Nardoni <nardoni59@gmail.com>
2018-04-27 14:40:32 -0700

    Make url work with apikey var

--- src/main.js
+++ src/main.js
@@ -2,23 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { doctor } from './../src/doctor.js';
-require('Dotenv').config();
 
 $(document).ready(function() {
   $('#showDr').click(function() {
     // event.preventDefault();
     let query = $('#findDr').val();
     $('#findDr').val("");
-    var apiKey = process.env.apiKey;
+    // let api_Key = $(process.env.apiKey);
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=5&user_key=apiKey`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        $('#output').append('<h5>'+`${response.data[0].profile.first_name}`+'</h5>');
-        $('#output').append('<h5>'+`${response.data[0].profile.last_name}`+'</h5>');
+        $('#output').append('<h5>'+`${response.data[0].profile.first_name} ${response.data[0].profile.last_name}`+'</h5>');
+        // $('#output').append('<h5>'+`${response.data[0].profile.last_name}`+'</h5>');
         $('#output').append(`${response.data[0].practices[0].visit_address.street}`);
         $('#output').append(`${response.data[0].practices[0].phones[0].number}`);
         $('#output').append(`${response.data[0].practices[0].accepts_new_patients}`);
