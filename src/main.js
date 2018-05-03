38c7c44854bfe0b8cda52f52fccc3c4cd123d6c7
Guest User <Guest@Epicodus-6D.local>
2018-05-02 19:52:08 -0700

    Replace success with .get

--- src/main.js
+++ src/main.js
@@ -5,33 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#showDr').click(function() {
-    // event.preventDefault();
     let query = $('#findDr').val();
     $('#findDr').val("");
     let name = $('#findName').val();
     $('#findName').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        for (var i = 0; i < `${response.data.length}`; i++) {
-          if (`${response.data[i]}` === 0) {
-            $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-          } else {
-            $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
-            $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[i].visit_address.street}` + '</h6>');
-            $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[i].phones[0].number}` + '</h6>');
-            $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[i].accepts_new_patients}` + "</b>" + '</h6>');
-            $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[i].website}` + '</h6>');
-          }
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response) {
+      for (let i = 0; i < `${response.data.length}`; i++) {
+        if (`${response.data[i]}` === null) {
+          $('#output').text("Sorry, we couldn't find anyone matching those terms.");
+        } else {
+          $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
+          $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[i].visit_address.street}` + '</h6>');
+          // $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[i].phones[0].number}` + '</h6>');
+          // $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[i].accepts_new_patients}` + "</b>" + '</h6>');
+          // $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[i].website}` + '</h6>');
         }
-      },
-      // .fail(function(error) {
-      //   $('#output').text(`There was an error processing your request: ${error.responseText}`);
-      // }
+      }
+    }).fail(function(error) {
+      $('#output').text(`There was an error processing your request: ${error.responseText}`);
     });
   });
 });
