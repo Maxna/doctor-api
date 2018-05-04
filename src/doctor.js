987e993635898d4e8ca3cc15da4db888a61ba5d7
Guest User <Guest@Epicodus-9E.local>
2018-05-03 21:32:10 -0700

    Split logic works

--- src/doctor.js
+++ src/doctor.js
@@ -1,11 +1,8 @@
 import $ from 'jquery';
 
 class API {
-  searchByIssue(query){
-    return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
-  }
-  searchByName(name){
-    return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
+  searchDoctor(name, query){
+    return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`);
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { API } from './../src/doctor.js';
+import { API } from './doctor.js';
 
 $(document).ready(function() {
-  let apiCall;
-  apiCall = new API();
   $('#formDr').submit(function(event) {
     event.preventDefault();
-    let query;
-    query = $('#findDr').val();
-    // $('#findDr').val("");
-    let issueResult = apiCall.searchByIssue(query);
-    issueResult.then(function(response){
+    let name = $('#findName').val();
+    $('#findName').val("");
+    let query = $('#findDr').val();
+    $('#findDr').val("");
+    let apiCall;
+    apiCall = new API();
+
+    let doctorResult = apiCall.searchDoctor(name, query);
+    doctorResult.then(function(response){
       for (let i = 0; i < `${response.data.length}`; i++) {
-        // if (`${response.data[i]}` === undefined) {
-        //   $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-        // } else {
+        if (`${response.data[i]}` === undefined) {
+          $('#output').text("Sorry, we couldn't find anyone matching those terms.");
+        } else {
         $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
         $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[0].visit_address.street}` + '</h6>');
         $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[0].phones[0].number}` + '</h6>');
         $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[0].accepts_new_patients}` + "</b>" + '</h6>');
         $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[1].website}` + '</h6>');
-        // }
+        }
       }
       // if (`${response.data.length}` === undefined) {
       //   $('#output').text("Sorry, we couldn't find anyone matching those terms.");
@@ -36,38 +38,7 @@ $(document).ready(function() {
       //   }
       // }
     }).fail(function(error){
-      $('#output').text(`There was an error processing your request: ${error.responseText}`);
-    });
-
-    let name;
-    name = $('#findName').val();
-    // $('#findName').val("");
-    let nameResult = apiCall.searchByName(name);
-    nameResult.then(function(response){
-      for (let i = 0; i < `${response.data.length}`; i++) {
-        // if (`${response.data[i]}` === undefined) {
-        //   $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-        // } else {
-        $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
-        $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[0].visit_address.street}` + '</h6>');
-        $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[0].phones[0].number}` + '</h6>');
-        $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[0].accepts_new_patients}` + "</b>" + '</h6>');
-        $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[1].website}` + '</h6>');
-        // }
-      }
-      // if (`${response.data.length}` === undefined) {
-      //   $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-      // }
-
-      // for (let i = 0; i < `${response.data.practices.length}`; i++) {
-      //   if (`${response.data.practices[1].website}` === undefined) {
-      //     $('#output').append('<h6>' + 'No Website Listed' + '</h6>');
-      //   } else {
-      //     $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[1].website}` + '</h6>');
-      //   }
-      // }
-    }).fail(function(error){
-      $('#output').text(`There was an error processing your request: ${error.responseText}`);
+      $('#errors').text(`There was an error processing your request: ${error.responseText}`);
     });
   });
 });
