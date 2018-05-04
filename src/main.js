70348f4c74bc7db234aafb0dfb10182d1ae92e53
Guest User <Guest@Epicodus-9E.local>
2018-05-03 22:28:08 -0700

    Add functionality to else output

--- src/main.js
+++ src/main.js
@@ -12,24 +12,22 @@ $(document).ready(function() {
     $('#findDr').val("");
     let apiCall;
     apiCall = new API();
-
+    // if (name === "" && query === "") {
+    //   alert("type something");
+    // }
     let doctorResult = apiCall.searchDoctor(name, query);
     doctorResult.then(function(response){
-      for (let i = 0; i < `${response.data.length}`; i++) {
-        if (`${response.data[i]}` === undefined) {
-          $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-        } else {
-        $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
-        $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[0].visit_address.street}` + '</h6>');
-        $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[0].phones[0].number}` + '</h6>');
-        $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[0].accepts_new_patients}` + "</b>" + '</h6>');
-        $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[1].website}` + '</h6>');
+      if ( `${response.meta.total}` >= 1 && (name !== "" || query !== "")) {
+        for (let i = 0; i < `${response.data.length}`; i++) {
+          $('#output').append('<h5>' + `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}` + '</h5>');
+          $('#output').append('<h6>' + 'Address: ' + `${response.data[i].practices[0].visit_address.street}` + '</h6>');
+          $('#output').append('<h6>' + 'Phone: ' + `${response.data[i].practices[0].phones[0].number}` + '</h6>');
+          $('#output').append('<h6>' + 'Currently accepting new patients: ' + "<b>" + `${response.data[i].practices[0].accepts_new_patients}` + "</b>" + '</h6><br>');
+          // $('#output').append('<h6>' + 'Website: ' + `${response.data[i].practices[1].website}` + '</h6>');
         }
+      } else {
+        $('#output').text("Sorry, we couldn't find anyone matching those terms.");
       }
-      // if (`${response.data.length}` === undefined) {
-      //   $('#output').text("Sorry, we couldn't find anyone matching those terms.");
-      // }
-
       // for (let i = 0; i < `${response.data.practices.length}`; i++) {
       //   if (`${response.data.practices[1].website}` === undefined) {
       //     $('#output').append('<h6>' + 'No Website Listed' + '</h6>');
